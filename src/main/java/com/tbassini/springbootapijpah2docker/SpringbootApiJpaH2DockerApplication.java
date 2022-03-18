b5137c48f0a4ff7abd88a25bf7da795a01d1ef8d
Tiago <tiagobassini@gmail.com>
2022-03-18 09:44:02 -0300

    commit do projeto inicial spring boot

---
+++ src/main/java/com/tbassini/springbootapijpah2docker/SpringbootApiJpaH2DockerApplication.java
@@ -0,0 +1,13 @@
+package com.tbassini.springbootapijpah2docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiJpaH2DockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiJpaH2DockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tbassini/springbootapijpah2docker/SpringbootApiJpaH2DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbassini.springbootapijpah2docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiJpaH2DockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
